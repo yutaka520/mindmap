7236d24792d9f7a3808643f7a9adfc005c5ad118
yutaka520 <yutakaofvita@gmail.com>
2026-06-24 14:33:31 +0900

    フッター修正

--- components/SiteFooter.tsx
+++ components/SiteFooter.tsx
@@ -7,6 +7,16 @@ export function SiteFooter() {
   return (
     <footer className="site-footer">
       <div className="site-footer__inner site-footer__inner--compact">
+        <div className="site-footer__meta">
+          <p className="site-footer__copyright">© {currentYear} {SITE_NAME}</p>
+          <p className="site-footer__credit">
+            created by{' '}
+            <a href="https://yutaka-create.com" target="_blank" rel="noreferrer">
+              Yutaka Tanabe
+            </a>
+          </p>
+        </div>
+
         <div className="site-footer__links">
           <Link href="/editor">編集</Link>
           <Link href="/terms">利用規約</Link>
